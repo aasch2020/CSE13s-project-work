4d88799d1603594d88cd573216c56452ea7e485a
Alex Asch <alasch@ucsc.edu>
2021-10-08 02:11:35 +0000

    adding sqrt newton prototype

---
+++ asgn2/newton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include "mathlib.h"
+static int counter = 0;
+double sqrt_newton(double input){
+	double previous = 0.0;
+	double guess = 1.0;
+	while (absolute(guess - previous) > EPSILON){
+		counter++;
+		previous = guess;
+		guess = 0.5*(previous+(input/previous));
+	}
+	return guess;
+}
+int sqrt_newton_iters(){
+	return counter;
+}	
