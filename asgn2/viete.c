d49e586f75d91abb4647e59662dc5a9896245a7d
Alex Asch <alasch@ucsc.edu>
2021-10-10 06:20:13 +0000

    reformat... I like pipeline to pass

--- asgn2/viete.c
+++ asgn2/viete.c
@@ -4,11 +4,11 @@
 static int counter = 0;
 double pi_viete() {
     double eout = 1.0;
-    double prev = sqrt_newton(2.0);
+    double prev = 0.0;
     counter = 0;
-    while (absolute(2 / prev - 1) > EPSILON) {
-        eout *= 2.0 / prev;
+    while (absolute(2.0 / prev - 1.0) > EPSILON) {
         prev = sqrt_newton(2.0 + prev);
+        eout *= 2.0 / prev;
         counter++;
     }
     return 2 * eout;
