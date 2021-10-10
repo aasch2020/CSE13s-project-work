25edc5b8cf1113bbb137d6f07db1c3945bf3b0a7
Alex Asch <alasch@ucsc.edu>
2021-10-10 05:08:55 +0000

    bbp ver 2

--- asgn2/bbp.c
+++ asgn2/bbp.c
@@ -7,19 +7,18 @@ double pi_bbp() {
     double term = 47.0 / 15;
     counter = 1;
     while (term > EPSILON) {
-        term = term * (1 / 16.0)
+         term = term * (1 / 16.0)
                * ((counter * (120.0 * counter + 151.0) + 47.0)
                    / (counter * (counter * (counter * (512.0 * counter + 1024.0) + 712.0) + 194.0)
                        + 15.0))
                * (((counter - 1.0)
                           * ((counter - 1.0)
                                   * ((counter - 1.0) * (512.0 * (counter - 1.0) + 1024.0) + 712.0)
-                              + 194.0)
-                      + 15.0)
+                              + 194.0) + 15.0)
                    / ((counter - 1.0) * (120.0 * (counter - 1.0) + 151.0) + 47.0));
         counter++;
         sum += term;
-    }
+   }
     return sum;
 }
 int pi_bbp_terms() {
