be6c5fdb7e2b82b6a2564d3069bae09a6f62d016
alasch <alasch@ucsc.edu>
2021-10-18 00:01:17 +0000

    comments and error handle

--- asgn3/heap.c
+++ asgn3/heap.c
@@ -1,56 +1,83 @@
 #include "stats.h"
-
+//stats.h is supplied by Professor Long in the resources repositoty (2021 Professor Long)
 #include <stdbool.h>
-#include <stdio.h>
+
+//This code is inspired by the python Python code in the Assignment document posted by Professor Long (2021 Professor Long)
+
+//The evaluation of indices in this program is done assuming 1 based indexing.
+//Thus, 1 is subtracted from all indices when used to get values from the array.
+
+//This function takes a stats struct, an Array of uint32_t, a parent index of uint32_t, and a last index of type uint32_t
+//It finds the largest child value of the given parent index.
 uint32_t maximum_child(Stats *stats, uint32_t *A, uint32_t parent_index, uint32_t last) {
     uint32_t left = 2 * parent_index;
-    //	printf("find kid of %d\n", A[parent_index - 1]);
     uint32_t right = left + 1;
+    //This statement makes sure that the right child is not off of the end of the list, and greater than the left child.
     if ((right <= last) && (cmp(stats, A[right - 1], A[left - 1]) == 1)) {
         return right;
     } else {
         return left;
     }
 }
+
+//This function takes a stats structe, an array of uint32_t, and a first and last index, both of type uint32_t.
+//This function makes the roots of the parent value given by first index follow the max heap constraints
+//This means for each element x  between the start and the end / 2, both A[x*2] and A[x*2+1} are less than x
+
 void fix_heap(Stats *stats, uint32_t *A, uint32_t first_index, uint32_t last) {
-    bool fixed = true;
-    //	printf("heap fix\n");
+    bool fixed = false;
     uint32_t parent = first_index;
     uint32_t greatest_child = maximum_child(stats, A, parent, last);
-    //printf("%u\n", (last / 2));
-    while ((parent <= (last / 2)) && fixed) {
+
+    //This loop iterate through the children of the given parent value.
+    //This loop stops iteration, when either the given parent has both children less than it,
+    //or if the given parent is the last parent value
+    //In essence, this loop makes sure that all of the children of a parent node follow max heap constraints
+
+    while ((parent <= (last / 2)) && !(fixed)) {
         if (cmp(stats, A[parent - 1], A[greatest_child - 1]) == -1) {
-            //			printf("fix swap %d, %d\n", A[parent - 1], A[greatest_child - 1]);
 
             swap(stats, &A[parent - 1], &A[greatest_child - 1]);
-            //			printf("fix swap %d, %d\n", A[parent - 1], A[greatest_child - 1]);
 
             parent = greatest_child;
             greatest_child = maximum_child(stats, A, parent, last);
         } else {
-            fixed = false;
+            fixed = true;
         }
     }
+    return;
 }
+
+//This function takes a stats struct, an array of uint32_t, and two indexes of first and last, both of type uint32_t
+//This function takes an unordered array and makes a max heap
+//This is achieved by calling fix_heap on all parent value in a reverse order.
+
 void build_heap(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
-    //	printf("heap build\n");
     for (uint32_t i = (last / 2); i >= first; i--) {
-        //		printf("build for%u\n",A[i-1]);
+
         fix_heap(stats, A, i, last);
-        //	printf("built heap%u, %u, %u, %u \n", A[0], A[1], A[2], A[3]);
     }
+    return;
 }
+
+//This function takes a stats struct, an array of uint32_t A, and uint32_t of the last index
+//This function sorts an array using heap sort.
+
 void heap_sort(Stats *stats, uint32_t *A, uint32_t n) {
     uint32_t first_ind = 1;
     uint32_t last = n;
     build_heap(stats, A, first_ind, n);
-    //	printf("postbld\n");
+    //This calls the build heap function, thus making the array a max heap
+    //
+    //The below loop loops through the array from last to first, swapping the two values each iteration
+    //In essence, this loop takes the first index, which in a max heap is the greatest value, and moves it to the end
+    //It then fixes the heap, by fixing all the children of the new first index, and then iterates again.
+    //Each iteration, it ignores the last value, effecfively shrinking the heap by one each time, and creating a sorted array.
     for (uint32_t i = last; i > first_ind; i--) {
-        //		printf("end swap %d, %d\n", A[first_ind - 1], A[i - 1]);
+
         swap(stats, &A[first_ind - 1], &A[i - 1]);
-        //	printf("end swap %d, %d\n", A[first_ind - 1], A[i - 1]);
-        //		printf("is gone now %d\n", A[i - 1]);
+
         fix_heap(stats, A, first_ind, (i - 1));
-        //			printf("iter heap final%u, %u, %u, %u \n", A[0], A[1], A[2], A[3]);
     }
+    return;
 }
--- asgn3/quick.c
+++ asgn3/quick.c
@@ -1,8 +1,19 @@
 #include "stats.h"
+//The stats header file is given by Professor Long in the resources repo (2021 Professor Long)
 
 #include <math.h>
+
+//the below code is adapted off of the python code given in the assignment document. (2021 Professor Long)
+
+//This function takes a stats structure, an array of uint_32t, with both the low and high indeces of the array.
+//The below function takes an array, and using a pivot,  creates two arrays
+//The two arrays are created in a way that the left array contains all values less than pivot, and the right is all values greater than.
+//This function returns the index of the division of the two arrays
 uint32_t partition(Stats *stats, uint32_t *A, uint32_t low, uint32_t high) {
     uint32_t i = low - 1;
+    //This loops over the array from the start to end, and if the current value is greater than the last
+    //Swaps the value at the pivot and the current index
+    //It then increments the pivot by one.
     for (uint32_t j = low; j < high; j++) {
         if (cmp(stats, A[j - 1], A[high - 1]) == -1) {
             i += 1;
@@ -13,13 +24,20 @@ uint32_t partition(Stats *stats, uint32_t *A, uint32_t low, uint32_t high) {
     return i + 1;
 }
 
+//This function takes a stats struct, an array of unit32_t, and indices of unit32_t indicating the beginning and end of the array.
+//This first paritions the array into two sub arrays, where everything to the left array is smaller than pivot, and to the right array is greater than
+//This function then calls itself on the two sublists, and repeats until there is a list of length one,which is sorted.
 void quick_sorter(Stats *stats, uint32_t *A, uint32_t low, uint32_t high) {
     if (low < high) {
         uint32_t p = partition(stats, A, low, high);
         quick_sorter(stats, A, low, p - 1);
         quick_sorter(stats, A, p + 1, high);
     }
+    return;
 }
+//This function takes a stats struct, an array of uin32_t, and the index of the end of the array n
+//Thi function is the start for the above recursive function, and intializes the value of the first index.
 void quick_sort(Stats *stats, uint32_t *A, uint32_t n) {
     quick_sorter(stats, A, 1, n);
+    return;
 }
