182c1b098da04139a2e407d913a3d17f2aa28b49
alasch <alasch@ucsc.edu>
2021-10-24 09:52:51 +0000

    asdfasdfasdf

--- asgn4/tsp.c
+++ asgn4/tsp.c
@@ -81,7 +81,7 @@ int main(int argc, char **argv) {
     char **city = (char **) calloc(number_vertices, sizeof(char *));
     for (uint32_t i = 0; i < number_vertices; i++) {
         fgets(buffer, 1024, input);
-	printf("%s", buffer);
+//	printf("%s", buffer);
         buffer[strlen(buffer) - 1] = '\0';
 //	fprintf(output,"%s", buffer);
 	//sscanf(buffer, "%s", city[i]);
@@ -109,7 +109,7 @@ int main(int argc, char **argv) {
 
     graph_delete(&graph);
     for (uint32_t i = 0; i < number_vertices; i++) {
-//	printf("%s", city[i]);
+	printf("%s", city[i]);
         free(city[i]);
     }
     free(city);
