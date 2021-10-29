51012125a4269c965fe0e045703c57222e676e34
alasch <alasch@ucsc.edu>
2021-10-29 23:55:38 +0000

    huffman

--- asgn5/huffman.c
+++ asgn5/huffman.c
@@ -1,1 +1,57 @@
+#include "pq.h"
+#include "node.h"
+#include <stdio.h>
 
+int main() {
+    printf("asdf\n");
+    printf("mainskis\n");
+    struct Node *nodeone = node_create('a', 5);
+    struct Node *nodetwo = node_create('x', 12);
+    struct Node *nodethree = node_join(nodeone, nodetwo);
+    struct Node *nodefour = node_join(nodethree, nodetwo);
+    struct PriorityQueue *pq = pq_create(13);
+    //   struct Node *nodetwo = node_create('x', 12);
+    struct Node *a = node_create('x', 2);
+    struct Node *b = node_create('x', 45);
+    struct Node *c = node_create('x', 3);
+    struct Node *d = node_create('x', 123);
+    struct Node *e = node_create('x', 4);
+    struct Node *f = node_create('s', 1);
+    enqueue(pq, f);
+    enqueue(pq, a);
+    enqueue(pq, b);
+    enqueue(pq, c);
+    enqueue(pq, d);
+    enqueue(pq, e);
+    // enqueue(pq, nodetwo);
+    // enqueue(pq, nodefour);
+    // enqueue(pq, nodeone);
+    //  enqueue(pq, nodethree);
+
+    pq_print(pq);
+
+    dequeue(pq, &a);
+    dequeue(pq, &b);
+    dequeue(pq, &c);
+    dequeue(pq, &d);
+    dequeue(pq, &e);
+    dequeue(pq, &f);
+    dequeue(pq, &nodeone);
+    pq_print(pq);
+    pq_delete(&pq);
+    node_delete(&nodefour);
+    node_delete(&nodeone);
+    node_delete(&nodetwo);
+    node_delete(&nodethree);
+    node_print(a);
+    node_print(b);
+    node_print(c);
+    node_print(d);
+    node_print(e);
+    node_print(f);
+    node_delete(&a);
+    node_delete(&b);
+    node_delete(&c);
+    node_delete(&d);
+    node_delete(&e);
+}
--- asgn5/pq.c
+++ asgn5/pq.c
@@ -51,21 +51,19 @@ uint32_t pq_size(PriorityQueue *q) {
 void node_swap(Node **x, Node **y) {
     Node *t = *x;
     *x = *y;
-
     *y = t;
     return;
 }
 
-uint32_t min_node_child(PriorityQueue *q, uint32_t parent_index, uint32_t end){
-	uint32_t left = 2*parent_index;
-	uint32_t right = left + 1;
-	if((right <= end)&&(((q->items[right - 1])->frequency) < ((q->items[left - 1])->frequency))){
-		return right;
-	}else{
-		return left;
-
-	}
-
+uint32_t min_node_child(PriorityQueue *q, uint32_t parent_index, uint32_t end) {
+    uint32_t left = 2 * parent_index;
+    uint32_t right = left + 1;
+    if ((right <= end)
+        && (((q->items[right - 1])->frequency) < ((q->items[left - 1])->frequency))) {
+        return right;
+    } else {
+        return left;
+    }
 }
 bool enqueue(PriorityQueue *q, Node *n) {
     if (pq_full(q)) {
@@ -78,7 +76,6 @@ bool enqueue(PriorityQueue *q, Node *n) {
         while ((current > 1) && !(fixed)) {
             if (((q->items[current - 1])->frequency) < ((q->items[(current / 2) - 1])->frequency)) {
                 node_swap(&q->items[current - 1], &q->items[(current / 2) - 1]);
-
             } else {
                 fixed = true;
             }
@@ -92,98 +89,38 @@ bool dequeue(PriorityQueue *q, Node **n) {
     if (pq_empty(q)) {
         return false;
     }
-    node_swap(&q->items[0],&q->items[q->top - 1]);
+    node_swap(&q->items[0], &q->items[q->top - 1]);
     q->top--;
     *n = q->items[q->top];
     uint32_t parent = 1;
     uint32_t least_child = min_node_child(q, parent, q->top);
     bool fixed = false;
-    while((parent <= (q->top/2)) && !(fixed)){
-		if(((q->items[parent - 1])->frequency) > ((q->items[least_child - 1])->frequency)){
-			node_swap(&q->items[parent - 1], &q->items[least_child - 1]);
-			parent = least_child;
-			least_child = min_node_child(q, parent, q->top);
+    while ((parent <= (q->top / 2)) && !(fixed)) {
+        if (((q->items[parent - 1])->frequency) > ((q->items[least_child - 1])->frequency)) {
+            node_swap(&q->items[parent - 1], &q->items[least_child - 1]);
+            parent = least_child;
+            least_child = min_node_child(q, parent, q->top);
 
-		}else{
-			fixed = true;
-		}
-	   }
+        } else {
+            fixed = true;
+        }
+    }
     return true;
 }
 
-
 void pq_print(PriorityQueue *q) {
-   printf("\n");
-   uint32_t cntr = 0;
-   uint32_t tree = 1;
-   for (uint32_t i = 0; i < q->top; i++) {
+    printf("\n");
+    uint32_t cntr = 0;
+    uint32_t tree = 1;
+    for (uint32_t i = 0; i < q->top; i++) {
         printf("           %lu             ", (q->items[i])->frequency);
-	cntr++;
-	if(cntr == tree){
-		printf("\n");
-		tree*=2;
-		cntr = 0;
-
-	}
-   }
-   printf("\n");
-   return;
-    
-}
-int main() {
-    printf("mainskis\n");
-    struct Node *nodeone = node_create('a', 5);
-    struct Node *nodetwo = node_create('x', 12);
-    struct Node *nodethree = node_join(nodeone, nodetwo);
-    struct Node *nodefour = node_join(nodethree, nodetwo);
-    struct PriorityQueue *pq = pq_create(13);
- //   struct Node *nodetwo = node_create('x', 12);
-struct Node *a = node_create('x', 2);
-struct Node *b = node_create('x', 45);
-struct Node *c = node_create('x', 3);
-struct Node *d = node_create('x', 123);
-struct Node *e = node_create('x', 4);
-struct Node *f = node_create('s', 1);
-enqueue(pq, f);
-enqueue(pq, a);
-enqueue(pq, b);
-	enqueue(pq, c);
-enqueue(pq, d);
-enqueue(pq, e);
-   // enqueue(pq, nodetwo);
-   // enqueue(pq, nodefour);
-   // enqueue(pq, nodeone);
-  //  enqueue(pq, nodethree);
-
-    pq_print(pq);
-
-    dequeue(pq, &a);
-    dequeue(pq, &b);
-    dequeue(pq, &c);
-     dequeue(pq, &d);
- dequeue(pq, &e);
- dequeue(pq, &f);
-dequeue(pq, &nodeone);
-    pq_print(pq);
-    pq_delete(&pq);
-    node_delete(&nodefour);
-    node_delete(&nodeone);
-    node_delete(&nodetwo);
-    node_delete(&nodethree);
-    node_print(a);
-   node_print(b); 
-    node_print(c);
-    node_print(d);
-    node_print(e);
-    node_print(f);
-     node_delete(&a);
-    node_delete(&b);
-    node_delete(&c);
-    node_delete(&d);
-  node_delete(&e);
-
-node_delete(&f);
-
-
-    printf("hit bot");
+        cntr++;
+        if (cntr == tree) {
+            printf("\n");
+            tree *= 2;
+            cntr = 0;
+        }
+    }
+    printf("\n");
+    return;
 }
