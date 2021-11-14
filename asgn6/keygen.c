6a4f75fb0b7b6e2aa723b2b48bd83fcde184cf45
alasch <alasch@ucsc.edu>
2021-11-14 00:05:22 +0000

    numthr

--- asgn6/keygen.c
+++ asgn6/keygen.c
@@ -2,8 +2,9 @@
 #include "numtheory.h"
 int main() {
     //	printf("main");
-    mpz_t a, d, b, c, f;
-    mpz_inits(a, d, b, c, f, NULL);
+    randstate_init(123);
+    mpz_t a, d, b, c, f, prm;
+    mpz_inits(a, d, b, c, f, prm, NULL);
     mpz_set_ui(a, 64);
     mpz_set_ui(b, 24);
     mpz_set_ui(c, 17);
@@ -15,4 +16,9 @@ int main() {
     mpz_set_ui(c, 64);
     mod_inverse(f, d, c);
     gmp_printf("The thing modinv %Zd\n", f);
+    mpz_init_set_ui(prm, 66);
+    printf("is prime %d\n", is_prime(prm, 25));
+    randstate_clear();
+    mpz_clears(a,b,c,d,f, prm,  NULL);
+
 }
--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -14,6 +14,7 @@ void gcd(mpz_t d, mpz_t a, mpz_t b) {
     }
 
     mpz_set(d, ac);
+    mpz_clears(ac, bc, temp, NULL);
     return;
 }
 
@@ -39,12 +40,15 @@ void mod_inverse(mpz_t i, mpz_t a, mpz_t n) {
     }
     if ((mpz_cmp_ui(r, 1) > 0)) {
         mpz_set_ui(i, 0);
+        mpz_clears(r, rz, t, tz, q, rtemp, ttemp, NULL);
         return;
     }
     if ((mpz_cmp_ui(t, 0) < 0)) {
         mpz_add(t, t, n);
     }
     mpz_set(i, t);
+    mpz_clears(r, rz, t, tz, q, rtemp, ttemp, NULL);
+    return;
 }
 
 void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus) {
@@ -62,16 +66,18 @@ void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus) {
         mpz_tdiv_q_2exp(exp, exp, 1);
     }
     mpz_set(out, v);
+    mpz_clears(v, p, NULL);
     return;
 }
 
 bool is_prime(mpz_t n, uint64_t iters) {
-    mpz_t r, s, n1, nrand, a, y, j, pm2;
+    mpz_t r, s, nz, nrand, a, y, j, pm2;
     mpz_inits(a, y, j, NULL);
+
     mpz_init_set_ui(pm2, 2);
-    mpz_init_set(n1, n);
+    mpz_init_set(nz, n);
     mpz_init_set(nrand, n);
-    mpz_sub_ui(n1, n1, 1);
+    mpz_sub_ui(nz, nz, 1);
     mpz_sub_ui(nrand, nrand, 3);
     mpz_init_set(r, n);
     mpz_sub_ui(r, r, 1);
@@ -84,25 +90,35 @@ bool is_prime(mpz_t n, uint64_t iters) {
         mpz_urandomm(a, state, nrand);
         mpz_add_ui(a, a, 2);
         pow_mod(y, a, r, n);
-        if ((mpz_cmp_ui(y, 1) != 0) && (mpz_cmp(y, n1))) {
+        if ((mpz_cmp_ui(y, 1) != 0) && (mpz_cmp(y, nz))) {
             mpz_set_ui(j, 1);
-            while ((mpz_cmp(j, s) < 0) && (mpz_cmp(y, n1) != 0)) {
+            while ((mpz_cmp(j, s) < 0) && (mpz_cmp(y, nz) != 0)) {
                 pow_mod(y, y, pm2, n);
                 if (mpz_cmp_ui(y, 1) == 0) {
+                    mpz_clears(r, s, nz, nrand, a, y, j, pm2, NULL);
                     return false;
                 }
                 mpz_add_ui(j, j, 1);
             }
-            if (mpz_cmp(y, n1) != 0) {
+            if (mpz_cmp(y, nz) != 0) {
+                mpz_clears(r, s, nz, nrand, a, y, j, pm2, NULL);
                 return false;
             }
         }
     }
+    mpz_clears(r, s, nz, nrand, a, y, j, pm2, NULL);
     return true;
 }
-/*
-void make_prime(mpz_t p, uint64_t bits, uint64_t iters){
 
+void make_prime(mpz_t p, uint64_t bits, uint64_t iters) {
+    mpz_t generated;
+    mpz_init(generated);
+    bool primefound = false;
+    while (!primefound) {
+        mpz_rrandomb(generated, state, bits);
+        primefound = is_prime(generated, iters);
+    }
+    mpz_set(p, generated);
+    mpz_clear(generated);
+    return;
 }
-
-*/
