69485744fa10649866951353e14b8873b131ca3b
alasch <alasch@ucsc.edu>
2021-11-15 06:51:36 +0000

    print->scan

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -69,7 +69,7 @@ void rsa_write_priv(mpz_t n, mpz_t d, FILE *pvfile) {
 
 void rsa_read_priv(mpz_t n, mpz_t d, FILE *pvfile) {
     gmp_fscanf(pvfile, "%Zx", n);
-    gmp_fprintf(pvfile, "%Zx", d);
+    gmp_fscanf(pvfile, "%Zx", d);
 }
 void rsa_encrypt(mpz_t c, mpz_t m, mpz_t e, mpz_t n) {
     pow_mod(c, m, e, n);
