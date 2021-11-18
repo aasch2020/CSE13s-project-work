df1c75a2f0bef12612c8e4efac24d3a602634446
alasch <alasch@ucsc.edu>
2021-11-18 02:09:44 +0000

    comment

--- asgn6/decrypt.c
+++ asgn6/decrypt.c
@@ -1,6 +1,7 @@
 #include "randstate.h"
 #include "numtheory.h"
 #include "rsa.h"
+//The above header files provided in the resources repository by Professor Long (2021 Professor Long)
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,10 +10,12 @@
 #include <sys/stat.h>
 #define OPTIONS "hvi:n:o:"
 
+//The belwo main function decrypts a given file with the given RSa private key
 int main(int argc, char **argv) {
     // The below code parses command line arguments,
     // It prints the appropriate help message if the h input is given
-    // It also sets the input and output files if they are given, and errors if the infile is invalid.
+    // It also sets the input and output files if they are given, and the private key file, and errors if the files are invalid.
+    // It also osets if the verbose output is specified
 
     int opt = 0;
 
@@ -27,33 +30,70 @@ int main(int argc, char **argv) {
         switch (opt) {
         case 'h':
             printf("help\n");
-
+            if (isoutfile) {
+                fclose(output);
+            }
+            if (isinfile) {
+                fclose(input);
+            }
+            if (isprivkey) {
+                fclose(privkey);
+            }
             return 0;
             break;
         case 'v': verb = true; break;
         case 'o':
             output = fopen(optarg, "w");
+            if (!output) {
+                printf("Failed to open output\n");
+                if (isinfile) {
+                    fclose(input);
+                }
+                if (isprivkey) {
+                    fclose(privkey);
+                }
+                return -1;
+            }
             isoutfile = true;
             break;
         case 'i':
             input = fopen(optarg, "r");
+            if (!input) {
+                printf("Failed to open input\n");
+                if (isoutfile) {
+                    fclose(output);
+                }
+                if (isprivkey) {
+                    fclose(privkey);
+                }
+            }
             isinfile = true;
             break;
         case 'n':
             privkey = fopen(optarg, "r");
-            isprivkey = true;
             if (!privkey) {
                 printf("Failed to open private key\n");
-                fclose(privkey);
+                if (isoutfile) {
+                    fclose(output);
+                }
+                if (isinfile) {
+                    fclose(input);
+                }
                 return -1;
             }
+
+            isprivkey = true;
+
             break;
         }
     }
+
+    //This code initializes the private key as the default rsa.priv if not specified.
     if (!isprivkey) {
         privkey = fopen("rsa.priv", "r");
     }
 
+    //This code reads the n and d from the private key and prints them if verbose output is specified
     mpz_t n, d;
     mpz_inits(n, d, NULL);
     rsa_read_priv(n, d, privkey);
@@ -62,6 +102,7 @@ int main(int argc, char **argv) {
         gmp_printf("d (%lu bits) = %Zd\n", mpz_sizeinbase(d, 2), d);
     }
 
+    //this code uses n and d to decrypt the input file to the output file.
     rsa_decrypt_file(input, output, n, d);
     fclose(privkey);
     mpz_clears(n, d, NULL);
@@ -71,4 +112,5 @@ int main(int argc, char **argv) {
     if (isinfile) {
         fclose(input);
     }
+    return 0;
 }
