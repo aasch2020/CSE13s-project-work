b555615cfeaf6c9bb20c910378ea7d9ce159e155
alasch <alasch@ucsc.edu>
2021-11-28 01:26:40 +0000

    bf

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -1,28 +1,27 @@
 #include "bv.h"
 #include "node.h"
 #include <stdio.h>
-
-int main(){
-	printf("main");
-	char test[] = "newspeaks";
-	char testing[] = "oldspeaks";
-	char asdf[] = "badspeaks";
-	Node *a = node_create(test, testing);
-	node_print(a);
-	test[3] = 'z';
-	node_print(a);
-	Node *b = node_create(asdf, NULL);
-	node_print(b);
-	BitVector *bv = bv_create(8);
-	bv_print(bv);
-	bv_set_bit(bv, 0);
-	bv_set_bit(bv, 6);
-	bv_print(bv);
-	bv_clr_bit(bv, 0);
-	bv_print(bv);
-	node_delete(&a);
-	node_delete(&b);
-	bv_delete(&bv);
-
-
+#include "bf.h"
+#include "speck.h"
+int main() {
+    printf("main");
+    char test[] = "newspeaks";
+    char testing[] = "oldspeaks";
+    char asdf[] = "badspeaks";
+    Node *a = node_create(test, testing);
+    node_print(a);
+    test[3] = 'z';
+    node_print(a);
+    Node *b = node_create(asdf, NULL);
+    node_print(b);
+    BitVector *bv = bv_create(8);
+    bv_print(bv);
+    bv_set_bit(bv, 0);
+    bv_set_bit(bv, 6);
+    bv_print(bv);
+    bv_clr_bit(bv, 0);
+    bv_print(bv);
+    node_delete(&a);
+    node_delete(&b);
+    bv_delete(&bv);
 }
--- asgn7/node.c
+++ asgn7/node.c
@@ -2,35 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-Node *node_create(char *oldspeak, char *newspeak){
-	Node *n = (Node *) calloc(1, sizeof(Node));
-	if(newspeak){
-		n->newspeak = strdup(newspeak);
-	}else{
+Node *node_create(char *oldspeak, char *newspeak) {
+    Node *n = (Node *) calloc(1, sizeof(Node));
+    if (newspeak) {
+        n->newspeak = strdup(newspeak);
+    } else {
 
-		n->newspeak = NULL;
-	}
-	n->oldspeak = strdup(oldspeak);
-	return n;
-
+        n->newspeak = NULL;
+    }
+    n->oldspeak = strdup(oldspeak);
+    return n;
 }
-void node_delete(Node **n){
-	if((*n)->oldspeak){
-		free((*n)->oldspeak);
-	}
-	if((*n)->newspeak){
-		free((*n)->newspeak);
-	}
-	free(*n);
-	*n=NULL;
-
+void node_delete(Node **n) {
+    if ((*n)->oldspeak) {
+        free((*n)->oldspeak);
+    }
+    if ((*n)->newspeak) {
+        free((*n)->newspeak);
+    }
+    free(*n);
+    *n = NULL;
 }
 
-void node_print(Node *n){
-	if(n->newspeak){
-		printf("%s -> %s\n", n->oldspeak , n->newspeak);
-	}else{
-		printf("%s\n", n->oldspeak);
-	}
-
+void node_print(Node *n) {
+    if (n->newspeak) {
+        printf("%s -> %s\n", n->oldspeak, n->newspeak);
+    } else {
+        printf("%s\n", n->oldspeak);
+    }
 }
