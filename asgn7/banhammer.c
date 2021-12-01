948fa2bc6b53643080e64f5e87255f635717f570
alasch <alasch@ucsc.edu>
2021-12-01 23:13:36 +0000

    fix regex?

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -4,9 +4,10 @@
 #include "bf.h"
 #include "speck.h"
 #include "bst.h"
+#include "regex.h"
 #include "ht.h"
 #include "parser.h"
-#define WORD "([a-zA-Z_0]+(['-]|[a-z_0-9A-Z])+[a-z]+)|([a-zA-Z])"
+#define WORD "([a-zA-Z_0]+(['-]|[a-z_0-9A-Z])[a-z]+)|([a-zA-Z]+)"
 int main() {
 
     Node *root = bst_create();
@@ -29,5 +30,20 @@ int main() {
     bf_print(bf);
     if (bf_probe(bf, "Zato")) {
         printf("worky");
+       regex_t re;
+    if (regcomp(&re, WORD, REG_EXTENDED)) {
+        fprintf(stderr, "Failed to compile regex.\n");
+        return 1;
+    }
+
+    char *word = NULL;
+    while ((word = next_word(stdin, &re)) != NULL) {
+        printf("Word: %s\n", word);
+    }
+
+    clear_words();
+    regfree(&re);
+    return 0;
+
     }
 }
