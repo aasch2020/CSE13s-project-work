000c81ef6aa32f7d1901c2c5cb4cff21a5fa832e
alasch <alasch@ucsc.edu>
2021-12-02 08:11:17 +0000

    banham

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -1,49 +1,96 @@
 #include "bv.h"
 #include "node.h"
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 #include "bf.h"
 #include "speck.h"
 #include "bst.h"
 #include "regex.h"
+#include "messages.h"
 #include "ht.h"
 #include "parser.h"
-#define WORD "([a-zA-Z0-9_]+(['-]|[a-z0-9A-Z_])+[a-zA_Z0-9_]+)|([a-zA-Z0-9_]+)"
+#define WORD "(([a-zA-Z0-9_]+(['-]|[a-z0-9A-Z_])+[a-zA_Z0-9_]+)|([a-zA-Z0-9_]+))"
 int main() {
 
-    Node *root = bst_create();
-    root = node_create("a", "b");
+    BloomFilter *bf = bf_create(1000000);
+    HashTable *ht = ht_create(60000);
+    FILE *badspeakfile;
+    badspeakfile = fopen("badspeak.txt", "r");
+    char badspk[1024];
+    while (EOF != fscanf(badspeakfile, "%s", badspk)) {
+        // printf("%s\n", badspk);
+        bf_insert(bf, badspk);
+        ht_insert(ht, badspk, NULL);
+    }
+    FILE *newspeakfile;
+    newspeakfile = fopen("newspeak.txt", "r");
+    char oldspk[1024];
+    char newspk[1024];
+    while (EOF != fscanf(newspeakfile, "%s %s", oldspk, newspk)) {
+        // printf("%s %s", oldspk, newspk);
+        bf_insert(bf, oldspk);
+        ht_insert(ht, oldspk, newspk);
+        if (!bf_probe(bf, oldspk)) {
+            printf("problem\n");
+        }
+        if (!ht_lookup(ht, oldspk)) {
+            printf("prbp\n");
+        }
+    }
+    //    ht_print(ht);
 
-    bst_insert(root, "a", "xd");
-    //    node_print(root);
-    bst_insert(root, "av", "Abba");
-
-    bst_insert(root, "am", "Abba");
-    bst_insert(root, "xd", "z");
-    // node_print(bst_find(root, "av" ));
-    node_print(bst_find(root, "xd"));
-
-    bst_print(root);
-    bst_delete(&root);
-    BloomFilter *bf = bf_create(30);
-
-    bf_insert(bf, "Zato");
-    bf_print(bf);
-    if (bf_probe(bf, "Zato")) {
-        printf("worky");
-       regex_t re;
-    if (regcomp(&re, WORD, REG_EXTENDED)) {
+    regex_t regex;
+    if (regcomp(&regex, WORD, REG_EXTENDED)) {
         fprintf(stderr, "Failed to compile regex.\n");
         return 1;
     }
-
+    Node *broot = NULL;
+    Node *oroot = NULL;
+    bool old = false;
+    bool bad = false;
     char *word = NULL;
-    while ((word = next_word(stdin, &re)) != NULL) {
-        printf("Word: %s\n", word);
-    }
+    Node *find = NULL;
+    while ((word = next_word(stdin, &regex)) != NULL) {
+        for (unsigned long i = 0; i < strlen(word); i++) {
+            word[i] = tolower(word[i]);
+        }
+        if (bf_probe(bf, word)) {
+            find = ht_lookup(ht, word);
+            if (find) {
+                if (find->newspeak) {
+                    old = true;
+                    if (!oroot) {
+                        oroot = node_create(find->oldspeak, find->newspeak);
+                    } else {
+                        bst_insert(oroot, find->oldspeak, find->newspeak);
+                    }
 
+                } else {
+                    if (!broot) {
+                        broot = node_create(find->oldspeak, find->newspeak);
+                    } else {
+                        bst_insert(broot, find->oldspeak, find->newspeak);
+                    }
+
+                    bad = true;
+                }
+            }
+        }
+    }
+    if (old & bad) {
+        printf("%s", mixspeak_message);
+    }
+    if (old & !bad) {
+        printf("%s", goodspeak_message);
+    }
+    if (bad & !old) {
+        printf("%s", badspeak_message);
+    }
+    bst_print(broot);
+    bst_print(oroot);
     clear_words();
-    regfree(&re);
+    regfree(&regex);
     return 0;
-
-    }
 }
